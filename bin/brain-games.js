c7009d1c7a7b321ae5d8415f36c5bbcf0e2ee166
andreyDanilovEd <andrey.danilov.ed@gmail.com>
2021-12-29 21:58:42 +0200

    add moduls

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import { userName } from '/Users/andrei/hexlet-git/frontend-project-lvl1/src/cli.js';
+import { name } from '/Users/andrei/hexlet-git/frontend-project-lvl1/src/cli.js';
 
-console.log('Welcome to the Brain Games!');
-console.log(userName(''));
+
+//console.log(name(''));
