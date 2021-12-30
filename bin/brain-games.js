61aa6ae8f649776fcfa17bade9b5deffcdb0ddf6
andreyDanilovEd <andrey.danilov.ed@gmail.com>
2021-12-30 20:11:12 +0200

    github actions

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 import { name } from '/Users/andrei/hexlet-git/frontend-project-lvl1/src/cli.js';
+console.log('Hello, ' + name + '!');
 
-
-//console.log(name(''));
+//console.log(name);
