fd73b3aac68c10febfcbb2c60ad49f147f8707e7
Andrey Danilov <andrey.danilov.ed@gmail.com>
2022-01-04 19:36:27 +0200

    update module src

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
-import { name } from '/Users/andrei/hexlet-git/frontend-project-lvl1/src/cli.js';
+import { name } from '/Users/andrei/frontend-project-lvl1/src/cli.js';
 console.log('Hello, ' + name + '!');
 
 //console.log(name);
+
