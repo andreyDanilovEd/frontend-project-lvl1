29cde36a166ff2ca424e0b8b4ba63fdee4e2e08b
Andrey Danilov <andrey.danilov.ed@gmail.com>
2022-01-11 22:17:00 +0200

    add random number at brain-ever

--- src/brain-even.js
+++ src/brain-even.js
@@ -11,22 +11,21 @@ const playBrainEven = () => {
         }
         return 'no';
     }
-    let questionNumber = 15;
+    let random = Math.floor(Math.random() * 10) + 1;
     for (let counter = 0; counter < 3; counter += 1) {
-        questionNumber = questionNumber + 1;
-        console.log('Question: ' + questionNumber);
+        //questionNumber = questionNumber + 1;
+        random = Math.floor(Math.random(random) * 10) + 1;
+        console.log('Question: ' + random);
         const userAnswer = readlineSync.question('Your answer: ');
         if (counter === 2) { 
-            console.log('Congratulation!');    
+            console.log('Congratulation, ' + name + '!');    
         } 
-        else if (userAnswer === brainEvenChecker(questionNumber)) {
+        else if (userAnswer === brainEvenChecker(random)) {
             console.log('Correct!');
         } 
-        else if (userAnswer !== brainEvenChecker(questionNumber)) {
+        else if (userAnswer !== brainEvenChecker(random)) {
             return console.log (userAnswer + ' is wrong answer' + '\nTry again, ' + name);
-            
         }
     }
 }       
-
 export default playBrainEven;
