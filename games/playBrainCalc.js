3bd485d73ac366fcb6cfc91699f717d60fc2e4d8
Andrey Danilov <andrey.danilov.ed@gmail.com>
2022-01-13 22:08:07 +0200

    Work version brain-calc

--- games/playBrainCalc.js
+++ games/playBrainCalc.js
@@ -8,23 +8,47 @@ const playCalc = () => {
     console.log('What is the result of the expression?');
 
     // функция для проверки ответа
+   const checkOperator = (operator) => {
 
-   
+    switch (operator) {
+        case '-':
+          // Делаем что-то полезное
+          let answer = random1 - random2;
+          return answer;
+          //break;
+        case '+':
+          // Делаем что-то полезное
+          let answer2 = random1 + random2;
+          return answer2;
+        case '*':
+          // Делаем что-то полезное
+          let answer3 = random1 * random2;
+          return answer3;
+        default:
+          // Что-то делаем
+      }
+   }   
+   let random1 = Math.floor(Math.random() * 10) + 1;
+   let random2 = Math.floor(Math.random() * 10) + 1;
     // функция для запуска 3 раундов
 for (let counter = 0; counter < 3; counter++) {
-    let random1 = Math.floor(Math.random() * 10) + 1;
-    let random2 = Math.floor(Math.random() * 10) + 1;
+    random1 = Math.floor(Math.random() * 10) + 1;
+    random2 = Math.floor(Math.random() * 10) + 1;
     const arr = ["*", "+", "-"];
-    let question = random1 + ' ' + arr[counter] + ' ' + random2
+    let question = random1 + ' ' + arr[counter] + ' ' + random2;
     console.log('Question: ' + question);
-    let operator = Array.reduce(arr);
-    console.log(operator);
-    let rightAnswer = Number(random1 + random2);
-    console.log(rightAnswer);
-
+    let operator = String(arr[counter]);
+    let rightAnswer = checkOperator(operator);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (counter === 2) { 
+        return 'Congratulation, ' + name + '!';    
+    } 
+    else if (Number(userAnswer) === rightAnswer) {
+        console.log('Correct!');
+    } 
+    else if (Number(userAnswer) !== rightAnswer) {
+        return "'" + userAnswer + "'" + ' is wrong answer ;(. Correct answer was ' + "'" + rightAnswer + "'" + `\nLet's try again, ` + name;
+    }
 }
-
-
-
 }
 export default playCalc;
