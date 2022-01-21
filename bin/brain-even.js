d787ebef4497603f7d2b5b4068de88fe3c18b28e
Andrey Danilov <andrey.danilov.ed@gmail.com>
2022-01-22 00:42:48 +0200

    fix linter problems

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
-import playBrainEven from '/Users/andrei/frontend-project-lvl1/games/brain-even.js';
+import playBrainEven from '../games/brain-even.js';
+
 console.log(playBrainEven());
-
-
-
