b827d8a36296e4deb46d50aa7c8f1869a4d10ebd
Andrey Danilov <andrey.danilov.ed@gmail.com>
2022-01-22 00:04:50 +0200

    linter

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
-import { name } from '/Users/andrei/frontend-project-lvl1/src/cli.js';
-console.log('Hello, ' + name + '!');
+import name from '../src/cli.js';
 
-//console.log(name);
-
+console.log(`Hello, ${name}!`);
