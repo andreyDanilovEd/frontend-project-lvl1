34177532f668c94bc0e7082ce04b0b770e1bc0f3
Andrey Danilov <andrey.danilov.ed@gmail.com>
2022-01-22 19:09:32 +0200

    fix linter problems

--- games/playBrain-prog.js
+++ games/playBrain-prog.js
@@ -1,48 +1,42 @@
 import readlineSync from 'readline-sync';
 
+// eslint-disable-next-line consistent-return
 const playBrainProg = () => {
-    console.log('Welcome to Brain games!');
-    const name = readlineSync.question('May I have your name? ');
-    console.log('Hello, ' + name + '!');
-    console.log('What number is missing in the progression?');
+  console.log('Welcome to Brain games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name} !`);
+  console.log('What number is missing in the progression?');
 
-    // функция для наполнения массива
-    
-    
-    let rightAnswer = [];
+  let rightAnswer = [];
 
-    const createRandomArr = (number, step) => {
-        let randomStepNumber = step;
-        
-        const randomArr = [];
-        randomArr.push(number);
-        for (let counter = 0; counter < 9; counter++) {
-            
-            if (counter < 9) {
-                randomArr.push(Number(randomArr[counter]) + Number(randomStepNumber));
-            }   
-            }
-            let forGap = `..`;
-            let randomIndex = Math.floor(Math.random() * 9) + 1;
-            rightAnswer = randomArr[randomIndex];
-            randomArr.splice(randomIndex, 1, forGap);
-            return randomArr;
-        }
-             // функция для запуска 3 раундов
- for (let counter = 0; counter < 3; counter++) {
-    let firstRandomNumber = Math.floor(Math.random() * 100) + 1;
-    let randomStepNumber = Math.floor(Math.random() * 10) + 2;
-    console.log('Question: ' + createRandomArr(firstRandomNumber, randomStepNumber));
-    let userAnswer = readlineSync.question('Your answer: ');
-    if (counter === 2) { 
-        return 'Congratulation, ' + name + '!';    
-    } 
-    else if (Number(userAnswer) === rightAnswer) {
-        console.log('Correct!');
-    } 
-    else if (Number(userAnswer) !== rightAnswer) {
-        return "'" + userAnswer + "'" + ' is wrong answer ;(. Correct answer was ' + "'" + rightAnswer + "'" + `\nLet's try again, ` + name;
+  const createRandomArr = (number, step) => {
+    const randomStepNumber = step;
+
+    const randomArr = [];
+    randomArr.push(number);
+    for (let counter = 0; counter < 9; counter += 1) {
+      if (counter < 9) {
+        randomArr.push(Number(randomArr[counter]) + Number(randomStepNumber));
+      }
     }
-}
-} 
+    const forGap = '..';
+    const randomIndex = Math.floor(Math.random() * 9) + 1;
+    rightAnswer = randomArr[randomIndex];
+    randomArr.splice(randomIndex, 1, forGap);
+    return randomArr;
+  };
+  for (let counter = 0; counter < 3; counter += 1) {
+    const firstRandomNumber = Math.floor(Math.random() * 100) + 1;
+    const randomStepNumber = Math.floor(Math.random() * 10) + 2;
+    console.log(`Question: ${createRandomArr(firstRandomNumber, randomStepNumber)}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (counter === 2) {
+      return `Congratulation, ${name}!`;
+    } if (Number(userAnswer) === rightAnswer) {
+      console.log('Correct!');
+    } else if (Number(userAnswer) !== rightAnswer) {
+      return `'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}\nLet's try again, ${name}`;
+    }
+  }
+};
 export default playBrainProg;
--- games/playBrainCalc.js
+++ games/playBrainCalc.js
@@ -1,53 +1,51 @@
 import readlineSync from 'readline-sync';
 
-
+// eslint-disable-next-line consistent-return
 const playCalc = () => {
-    console.log('Welcome to Brain games!');
-    const name = readlineSync.question('May I have your name? ');
-    console.log('Hello, ' + name + '!');
-    console.log('What is the result of the expression?');
+  console.log('Welcome to Brain games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name} !`);
+  console.log('What is the result of the expression?');
+  let random1 = Math.floor(Math.random() * 10) + 1;
+  let random2 = Math.floor(Math.random() * 10) + 1;
 
-    // функция для проверки ответа
-   const checkOperator = (operator) => {
-       let answer = 0;
-       let answer2 = 0;
-       let answer3 = 0;
+  // eslint-disable-next-line consistent-return
+  const checkOperator = (operator) => {
+    let answer = 0;
+    let answer2 = 0;
+    let answer3 = 0;
 
     switch (operator) {
-        case '-':
-          answer = random1 - random2;
-          return answer;
-        case '+':
-          answer2 = random1 + random2;
-          return answer2;
-        case '*':
-          answer3 = random1 * random2;
-          return answer3;
-        default:
-      }
-   }   
-   let random1 = Math.floor(Math.random() * 10) + 1;
-   let random2 = Math.floor(Math.random() * 10) + 1;
-    // функция для запуска 3 раундов
-    const arr = [" ", "*", "+", "-"];
-for (let counter = 0; counter < 3; counter++) {
+      case '-':
+        answer = random1 - random2;
+        return answer;
+      case '+':
+        answer2 = random1 + random2;
+        return answer2;
+      case '*':
+        answer3 = random1 * random2;
+        return answer3;
+      default:
+    }
+  };
+
+  const arr = [' ', '*', '+', '-'];
+  for (let counter = 0; counter < 3; counter += 1) {
     random1 = Math.floor(Math.random() * 10) + 1;
     random2 = Math.floor(Math.random() * 10) + 1;
-    let randomOperatorIndex = Math.floor(Math.random() * 3) + 1;
-    let operator = arr[randomOperatorIndex];
-    let question = random1 + ' ' + operator + ' ' + random2;
-    console.log('Question: ' + question);
-    let rightAnswer = checkOperator(operator);
+    const randomOperatorIndex = Math.floor(Math.random() * 3) + 1;
+    const operator = arr[randomOperatorIndex];
+    const question = `${random1} ${operator} ${random2}`;
+    console.log(`Question: ${question}`);
+    const rightAnswer = checkOperator(operator);
     const userAnswer = readlineSync.question('Your answer: ');
-    if (counter === 2) { 
-        return 'Congratulation, ' + name + '!';    
-    } 
-    else if (Number(userAnswer) === rightAnswer) {
-        console.log('Correct!');
-    } 
-    else if (Number(userAnswer) !== rightAnswer) {
-        return "'" + userAnswer + "'" + ' is wrong answer ;(. Correct answer was ' + "'" + rightAnswer + "'" + `\nLet's try again, ` + name;
+    if (counter === 2) {
+      return `Congratulation, ${name}!`;
+    } if (Number(userAnswer) === rightAnswer) {
+      console.log('Correct!');
+    } else if (Number(userAnswer) !== rightAnswer) {
+      return `'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}' \nLet's try again, ${name}`;
     }
-}
-}
+  }
+};
 export default playCalc;
