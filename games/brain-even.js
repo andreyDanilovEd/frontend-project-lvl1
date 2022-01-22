3489d27f05304416dda84b5c4f12040ce69ac5c2
Andrey Danilov <andrey.danilov.ed@gmail.com>
2022-01-22 18:22:14 +0200

    fix linter problems

--- games/brain-even.js
+++ games/brain-even.js
@@ -1,30 +1,28 @@
 import readlineSync from 'readline-sync';
 
 const playBrainEven = () => {
-    console.log('Welcome to Brain games!');
-    const name = readlineSync.question('May I have your name? ');
-    console.log('Hello, ' + name + '!');
-    console.log('Answer "yes" if the number is even, otherwise answer "no".');
-    const brainEvenChecker = (number) => {
-        if (number % 2 === 0){
-            return 'yes';
-        }
-        return 'no';
+  console.log('Welcome to Brain games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  const brainEvenChecker = (number) => {
+    if (number % 2 === 0) {
+      return 'yes';
     }
-    let random = Math.floor(Math.random() * 10) + 1;
-    for (let counter = 0; counter < 3; counter += 1) {
-        random = Math.floor(Math.random() * 10) + 1;
-        console.log('Question: ' + random);
-        const userAnswer = readlineSync.question('Your answer: ');
-        if (counter === 2) { 
-            return 'Congratulation, ' + name + '!';    
-        } 
-        else if (userAnswer === brainEvenChecker(random)) {
-            console.log('Correct!');
-        } 
-        else if (userAnswer !== brainEvenChecker(random)) {
-            return userAnswer + ' is wrong answer' + '\nTry again, ' + name;
-        }
+    return 'no';
+  };
+  let random = Math.floor(Math.random() * 10) + 1;
+  for (let counter = 0; counter < 3; counter += 1) {
+    random = Math.floor(Math.random() * 10) + 1;
+    console.log(`Question: ${random}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (counter === 2) {
+      return `Congratulation, ${name}!`;
+    } if (userAnswer === brainEvenChecker(random)) {
+      console.log('Correct!');
+    } else if (userAnswer !== brainEvenChecker(random)) {
+      return `${userAnswer} is wrong answer \nTry again,  ${name}`;
     }
-}       
+  }
+};
 export default playBrainEven;
--- games/playBrain-gcd.js
+++ games/playBrain-gcd.js
@@ -1,48 +1,46 @@
+/* eslint-disable consistent-return */
 import readlineSync from 'readline-sync';
 
-const playBrainGcd = () => {
+function playBrainGcd() {
+  console.log('Welcome to Brain games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name} !`);
+  console.log('Find the greatest common divisor of given numbers.');
+  let random1 = Math.floor(Math.random() * 50) + 1;
+  let random2 = Math.floor(Math.random() * 10) + 1;
 
-    console.log('Welcome to Brain games!');
-    const name = readlineSync.question('May I have your name? ');
-    console.log('Hello, ' + name + '!');
-    console.log('Find the greatest common divisor of given numbers.');
-    // цикл для поиска делителя
-    const findDivider = (a, b) => {
-        const toNumberA = Number(a);
-        const toNumberB = Number(b);
-        let maxRandomNumber = 0;
-        if (toNumberA > toNumberB) {
-            maxRandomNumber = toNumberA;
-        }
-        else if (toNumberA < toNumberB) {
-            maxRandomNumber = toNumberB;
-        }
-        for (let counter = maxRandomNumber; counter > 0; counter--) {
-            let maxDivider = 1;
-            if (random1 % counter === 0 && random2 % counter === 0) {
-                maxDivider = counter;
-                return maxDivider;
-            }
-        }
+  const findDivider = (a, b) => {
+    const toNumberA = Number(a);
+    const toNumberB = Number(b);
+    let maxRandomNumber = 0;
+    if (toNumberA > toNumberB) {
+      maxRandomNumber = toNumberA;
+    } else if (toNumberA < toNumberB) {
+      maxRandomNumber = toNumberB;
     }
-    let random1 = Math.floor(Math.random() * 50) + 1;
-    let random2 = Math.floor(Math.random() * 10) + 1;
-// цикл для 3 раундов
-for (let counter = 0; counter < 3 ; counter++) {
+    for (let counter = maxRandomNumber; counter > 0; counter -= 1) {
+      let maxDivider = 1;
+      if (random1 % counter === 0 && random2 % counter === 0) {
+        maxDivider = counter;
+        return maxDivider;
+      }
+    }
+  };
+
+  // eslint-disable-next-line no-plusplus
+  for (let counter = 0; counter < 3; counter += 1) {
     random1 = Math.floor(Math.random() * 50) + 1;
     random2 = Math.floor(Math.random() * 10) + 1;
-    console.log('Question: ' + random1 + ' ' + random2);
-    let rightDivider = findDivider(random1, random2);
+    console.log(`Question: ${random1} ${random2}`);
+    const rightDivider = findDivider(random1, random2);
     const userAnswer = readlineSync.question('Your answer: ');
-    if (counter === 2) { 
-        return 'Congratulation, ' + name + '!';    
-    } 
-    else if (Number(userAnswer) === rightDivider) {
-        console.log('Correct!');
-    } 
-    else if (Number(userAnswer) !== rightDivider) {
-        return "'" + userAnswer + "'" + ' is wrong answer ;(. Correct answer was ' + "'" + rightDivider + "'" + `\nLet's try again, ` + name;
+    if (counter === 2) {
+      return `Congratulation, ${name} !`;
+    } if (Number(userAnswer) === rightDivider) {
+      console.log('Correct!');
+    } else if (Number(userAnswer) !== rightDivider) {
+      return `'${userAnswer}' is wrong answer ;(. Correct answer was '${rightDivider}' \nLet's try again, ${name}`;
     }
-}
+  }
 }
 export default playBrainGcd;
