baccd385d178079201d38ca99d14230a99c11596
Andrey Danilov <andrey.danilov.ed@gmail.com>
2022-01-28 02:07:17 +0200

    test

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -2,4 +2,5 @@
 import { gameRule, getQuestionAndAnswer } from '../games/brain-even.js';
 import gameEngine from '../src/index.js';
 
+console.log('Congratulations, Tirion!');
 gameEngine(gameRule, getQuestionAndAnswer);
