bdf59341675449f8d9dfb6a166a26ddebd1f4051
Andrey Danilov <andrey.danilov.ed@gmail.com>
2022-02-01 23:49:36 +0200

    modify brain-prime

--- games/playPrime.js
+++ games/playPrime.js
@@ -4,21 +4,26 @@ const gameRule = 'Answer "yes" if the number is prime, otherwise answer "no".';
 
 const checkNumber = (num) => {
   let result = 0;
-  let resultCheck = 'yes';
   for (let counter = num; counter > 0; counter -= 1) {
     if (num % counter === 0) {
       result += 1;
     }
     if (result > 2 || num === 1) {
-      resultCheck = 'no';
+      return false;
     }
   }
-  return resultCheck;
+  return true;
 };
 const getQuestionAndAnswer = () => {
   const randomNumber = randomNum(1, 100);
-  const rightAnswer = checkNumber(randomNumber);
   const question = randomNumber;
+  const resultCheck = checkNumber(randomNumber);
+  let rightAnswer;
+  if (resultCheck === true) {
+    rightAnswer = 'yes';
+    return [question, rightAnswer];
+  }
+  rightAnswer = 'no';
   return [question, rightAnswer];
 };
 export { gameRule, getQuestionAndAnswer };
