bfedfb2b07dfb3e48259acea894a6bb78662d5ed
Andrey Danilov <andrey.danilov.ed@gmail.com>
2022-02-02 03:47:07 +0200

    modify playBrain-gcd

--- src/games/playBrain-gcd.js
+++ src/games/playBrain-gcd.js
@@ -4,29 +4,18 @@ import gameEngine from '../index.js';
 const gameRule = 'Find the greatest common divisor of given numbers.';
 
 const findDivider = (a, b) => {
-  const toNumberA = Number(a);
-  const toNumberB = Number(b);
-  let maxRandomNumber = 0;
-  if (toNumberA > toNumberB) {
-    maxRandomNumber = toNumberA;
-  } else if (toNumberA < toNumberB) {
-    maxRandomNumber = toNumberB;
-  } else if (toNumberB === toNumberA) {
-    maxRandomNumber = toNumberA;
+  let maxDivider;
+  maxDivider = a % b;
+  if (a === 0 || b === 0) {
+    maxDivider = a % b;
+    return a + b;
   }
-  let maxDivider = 1;
-  for (let counter = maxRandomNumber; counter > 0; counter -= 1) {
-    if (toNumberA % counter === 0 && toNumberB % counter === 0) {
-      maxDivider = counter;
-      return maxDivider;
-    }
-  }
-  return maxDivider;
+  return findDivider(b, maxDivider);
 };
 
 const getQuestionAndAnswer = () => {
-  const random1 = randomNum(1, 100);
-  const random2 = randomNum(1, 100);
+  const random1 = randomNum(50, 100);
+  const random2 = randomNum(1, 49);
   const question = `${random1} ${random2}`;
   const rightAnswer = findDivider(random1, random2).toString();
   return [question, rightAnswer];
